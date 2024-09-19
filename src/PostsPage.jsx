94d3fe98c0a031715840b1c5db84f6f1edf2df8d
Zack <168221270+Zval321@users.noreply.github.com>
2024-09-18 19:40:41 -0500

    added functionality to connect front & backend (react to rails)

--- src/PostsPage.jsx
+++ src/PostsPage.jsx
@@ -1,33 +1,19 @@
 import { PostsIndex } from "./PostsIndex";
 import { PostsNew } from "./PostsNew";
+import axios from "axios";
 
 export function PostsPage() {
-  const name = "Test";
-  const posts = [
-    {
-      id: 1,
-      title: "Hello!",
-      body: "Welcome to the first blog post!",
-      image: "https://cdn.dribbble.com/users/720738/screenshots/3848385/media/6e20ee860448d9181010d652892aba33.gif",
-    },
-    {
-      id: 2,
-      title: "Welcome!",
-      body: "Hope all is well, welcome to the blog!",
-      image: "https://t3.ftcdn.net/jpg/02/41/32/08/360_F_241320835_Z2fuURdWsRgtTnkARGQqiorzH8p4fnE5.jpg",
-    },
-    {
-      id: 3,
-      title: "How are we!?",
-      body: "Bienvenidos a mi blog!",
-      image: "https://www.creativefabrica.com/wp-content/uploads/2018/09/Howdy-Cowboy-by-Studio-26-Design-Co-1-580x387.jpg",
-    },
-  ];
-
+  const posts = [];
+  const handleIndex = () => { 
+    axios.get("http://localhost:3000/posts.json").then(response => { 
+      console.log(response);
+    });
+  }
   return (
     <div>
       <PostsIndex name={name} posts={posts} />
       <PostsNew />
+      <button onClick={handleIndex}>Get Data</button>
     </div>
   );
 }
